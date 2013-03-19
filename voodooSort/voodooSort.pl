927d1e7a0e45cd8429eff1a9c35b17cd690889a6
Leon Spaans <lspaans@gmail.com>
2013-03-19 11:30:15 +0100

    Removed some overhead from the main 'voodooSort'-routine.

--- voodooSort/voodooSort.pl
+++ voodooSort/voodooSort.pl
@@ -3,27 +3,51 @@
 use strict;
 use warnings;
 
+#-----------------------------------------------------------------------------#
+# Function      : voodooSort(;@)
+# Dependencies  :
+# Purpose       : A sorting algorithm for sorting 2d arrays in a  non-regular
+#                 way by indicating the column order to use and the type of
+#                 comparison (numerical or lexicographical) that should be used
+#                 while sorting.
+#
+# In  : [1] An optional array containing sort indicators. Sort indicators
+#           instruct the sorting algorithm in which order to sort the columns
+#           of the referenced arrays as contained within the array that is
+#           being sorted.
+#           Sort indicator syntax: '<sort type>:<col.nr>'.
+#           '<sort type>' = 'n' (numerical) or 's' (lexicographical).
+#           '<col.nr>' = positive integer value indicating the array column
+#           (0=col 1, 1=col 2 etc.)
+#
+# Out : [1]
+#
+#-----------------------------------------------------------------------------#
+
 sub voodooSort(;@) {
-  my (@cData,@c,@out)   = @_;
+  my (@cData,@c)   = @_;
   our ($a,$b);
-  for my $cDataEntry (@cData) {
-      if ($cDataEntry =~ /^([sn]):(\d+)$/g) {
+  for(@cData) {
+      if (/^([sn]):(\d+)$/g) {
           push @c,[$1,$2];
       } else {
           return(0);
       }
   }
-  @out = grep {$_ ne '0'} map {
+  return(
     (
-        defined $$a[$$_[1]] and
-        defined $$b[$$_[1]]
-    ) ? (
-        ( $$_[0] eq 's' or $$_[0] ne 'n' ) ?
-        $$a[$$_[1]] cmp $$b[$$_[1]] : 
-        $$a[$$_[1]] <=> $$b[$$_[1]]
-    ) : defined $$a[$$_[1]] ?  1 : -1
-  } @c;
-  return($out[0]||0);
+      grep {$_ ne '0'} map {
+      (
+          defined $$a[$$_[1]] and
+          defined $$b[$$_[1]]
+      ) ? (
+          ( $$_[0] eq 's' or $$_[0] ne 'n' ) ?
+          $$a[$$_[1]] cmp $$b[$$_[1]] : 
+          $$a[$$_[1]] <=> $$b[$$_[1]]
+      ) : defined $$a[$$_[1]] ?  1 : -1
+    } @c
+    )[0]||0
+  );
 }
 
 my @zut     = (
