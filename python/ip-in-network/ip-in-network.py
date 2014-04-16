356028b6587833dcde8d96dbf9be5d1b2b043360
Leon Spaans <lspaans@gmail.com>
2014-04-16 08:10:58 +0200

    Changed 'ip-in-network.py'. Replaced the "dirty" trick with the contructed bit-string.

--- python/ip-in-network/ip-in-network.py
+++ python/ip-in-network/ip-in-network.py
@@ -6,16 +6,20 @@ import struct
 def ip_in_network(mask, address):
 
     mask_address, mask_bits = mask.split("/",1)
+    bit_val     = 2 ** 31
+    mask_val    = 0
+
+    for n in xrange(int(mask_bits)):
+        if bit_val < 1:
+            break
+        mask_val += bit_val
+        bit_val /= 2
 
     mask_map    = socket.inet_ntoa(
         struct.pack(
-            "!I", struct.unpack("!I", socket.inet_aton( address )
-        )[0] & int(
-                "{0}{1}".format(
-                    '1' * int(mask_bits),
-                    '0' * (32 - int(mask_bits))
-                ), 2
-            )
+            "!I", struct.unpack(
+                "!I", socket.inet_aton(address)
+            )[0] & mask_val
         )
     )
 
@@ -24,4 +28,4 @@ def ip_in_network(mask, address):
 if __name__ == '__main__':
     print ip_in_network("10.0.0.0/8", "10.1.2.3")
     print ip_in_network("10.0.0.0/16", "10.1.2.3")
-
+    print ip_in_network("192.168.0.0/16", "192.168.1.2")
